0f03141d76d6e8cfcfd5f891f038c0ff299542fa
Osyfliak Ivan <osuflak22@gmail.com>
2022-12-30 18:30:37 +0200

    1 завдання

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const refs = {
+    buttonStart: document.querySelector('[data-start]'),
+    buttonStop: document.querySelector('[data-stop]'),
+    body: document.querySelector('body')
+}
 
+refs.buttonStop.disabled = true;
+refs.buttonStart.addEventListener('click', onTabButtonStart);
+refs.buttonStop.addEventListener('click', onTabButtonStop);
+
+function onTabButtonStart() {
+    refs.buttonStart.disabled = true;
+    refs.buttonStop.disabled = false;
+
+    timerId = setInterval(() => {
+        refs.body.style.backgroundColor = getRandomHexColor();
+    }, 1000); 
+}
+
+function onTabButtonStop() {
+    refs.buttonStart.disabled = false;
+    refs.buttonStop.disabled = true;
+
+    clearInterval(timerId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
