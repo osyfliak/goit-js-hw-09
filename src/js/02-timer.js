4e942656fcb98cc30150e2d7d69b1a3e6ce5803d
Osyfliak Ivan <osuflak22@gmail.com>
2022-12-30 20:11:28 +0200

    завдання 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,3 +1,56 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
+
+
+
+const refs = {
+    buttonStart: document.querySelector('[data-start]'),
+    days: document.querySelector('[data-dayst]'),
+    hours: document.querySelector('[data-hours]'),
+    minutes: document.querySelector('[data-minutes]'),
+    seconds: document.querySelector('[data-seconds]')
+}
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+      const currentDate = new Date();
+      if (selectedDates[0] < currentDate) {
+          Notify.failure("Please choose a date in the future");
+          refs.buttonStart.disabled = true;
+      } else {
+          refs.buttonStart.disabled = false;
+           console.log(selectedDates[0]);
+      }
+  },
+};
+
+flatpickr("#datetime-picker", options);
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+
+
+
 
